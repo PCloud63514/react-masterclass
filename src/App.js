1c4e06a445f82fdba38a60500bfa18429e64a874
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-04 20:07:49 +0900

    feat: 애니메이션 기능-3

--- src/App.js
+++ src/App.js
@@ -22,13 +22,28 @@ const Box = styled.div`
   height: 100px;
   width: 100px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   animation:${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    // & <= 자기 자신 (원래 css 라면 span:hover 형태)
+    &:hover {
+      font-size: 150px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `
 
 function App() {
     return (
         <Wrapper>
-            <Box/>
+            <Box>
+                <span>🚀</span>
+            </Box>
         </Wrapper>
     )
 }
