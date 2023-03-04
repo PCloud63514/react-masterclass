9c54ed7c2f91e60aac902639934ede9f8151706c
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-04 19:27:48 +0900

    feat: 내용 변경

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
