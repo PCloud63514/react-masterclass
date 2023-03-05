1988ad08a75ea64cc0b4ebb648c415f048841cac
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-05 18:07:40 +0900

    feat: react-query, react-router-dom 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ThemeProvider} from "styled-components";
-import {darkTheme} from "./theme";
+import {theme} from "./theme";
 
 
 const root = ReactDOM.createRoot(
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={theme}>
             <App/>
         </ThemeProvider>
     </React.StrictMode>
