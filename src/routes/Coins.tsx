71104c04cc47749bcf69e17657c9c46e174226da
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-05 19:09:55 +0900

    feat: react-router-dom Link 활용 및 프론트 페이지 추가

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,82 @@
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
+const Container = styled.div`
+  padding: 0 20px;
+`
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const CoinList = styled.ul``
+const Coin = styled.li`
+  background-color: white;
+  color: ${props => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    color: ${props => props.theme.accentColor}
+  }
+`
 const Title = styled.h1`
+  font-size: 48px;
   color: ${props => props.theme.accentColor}
 `
 
+const coins = [
+    {
+        id: "btc-bitcoin",
+        name: "Bitcoin",
+        symbol: "BTC",
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "eth-ethereum",
+        name: "Ethereum",
+        symbol: "ETH",
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "hex-hex",
+        name: "HEX",
+        symbol: "HEX",
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: "token",
+    },
+]
+
 function Coins() {
-    return <Title>Coins</Title>
+    return (
+        <Container>
+            <Header>
+                <Title>코인</Title>
+            </Header>
+            <CoinList>
+                {coins.map(coin =>
+                    <Coin key={coin.id}>
+                        <Link to={`/${coin.id}`}>
+                            {coin.name} &rarr;
+                        </Link>
+                    </Coin>)
+                }
+            </CoinList>
+        </Container>
+    )
 }
 
 export default Coins
