0b15a2d8ddf698397cca9bb529f943c850357dc5
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-05 16:51:36 +0900

    feat: styled-components에 d.ts 파일을 적용해서 타입을 오버라이드

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+  width: 100vw;
+  height: 100vh;
+`
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor}
+`
 
 function App() {
-    const [value, setValue] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setValue(value)
-    }
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("Hello", value)
-    }
-
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={value}
-                    onChange={onChange}
-                    type={"text"}
-                    placeholder={"username"}
-                />
-                <button>Log in</button>
-            </form>
-        </div>
+        <Container>
+            <H1>Protected</H1>
+        </Container>
     )
 }
 
