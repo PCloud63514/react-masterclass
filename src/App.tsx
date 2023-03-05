26e896a67ed59f684ea7de1bdaaac46b41b2b8a6
TAE HEON KWON <PCloud63514@gmail.com>
2023-03-05 18:15:14 +0900

    feat: react-router-dom v5 및 useParams 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-  width: 100vw;
-  height: 100vh;
-`
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor}
-`
+import Router from "./Router";
 
 function App() {
     return (
-        <Container>
-            <H1>Protected</H1>
-        </Container>
+        <Router/>
     )
 }
 
